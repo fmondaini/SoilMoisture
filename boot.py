f131cd4ba464a98554209750f910f498d39abc54
Filipe Mondaini <filipe@mondaini.me>
2017-06-04 22:02:27 -0300

    Adiciona boot.py (para o micropython)

---
+++ boot.py
@@ -0,0 +1,10 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+import esp
+import gc
+import webrepl
+
+esp.osdebug(None)
+esp.sleep_type(esp.SLEEP_LIGHT)
+
+webrepl.start()
+gc.collect()
