afff2e12f883b446cc00203b6de1db2f521184aa
Filipe Mondaini <filipe@mondaini.me>
2017-06-04 22:01:52 -0300

    Refatora o código

--- main.py
+++ main.py
@@ -33,47 +33,60 @@ class Service(object):
     """
     def __init__(self, *args, **kwargs):
         self.pin = Pin(2, Pin.OUT)
+        self.adc = ADC(0)
+        self.sta_if = network.WLAN(network.STA_IF)
+        self.sta_if.active(True)
         super(Service, self).__init__(*args, **kwargs)
 
-    def _toggle(pin):
-        pin.value(not pin.value())
+    def _toggle(self):
+        self.pin.value(not self.pin.value())
 
-    def get_sensor_data():
+    def get_sensor_data(self):
         """
         returns Report
         """
-        adc = ADC(0)
         data = {
-            "message": adc.read(),
+            "message": self.adc.read(),
             # "sender": Report.SENSOR
             "sender": "sensor",
         }
         return Report(**data)
 
-    def get_battery_status():
+    def get_battery_status(self):
         """
         returns: Report
         """
         return Report()
 
-    def connect_ap(sta_if):
+    def connect(self):
         """
         Connects to the AP
         Returns boolean of the connection attempt.
         """
-        sta_if.connect(
+        self.sta_if.connect(
             CONFIG['wifi']['essid'],
             CONFIG['wifi']['password'])
 
-    def led_status(self):
+        return self.is_connected()
+
+    def is_connected(self):
+        return self.sta_if.isconnected()
+
+    def status(self):
         """
         Report status using the built-in led from the NodeMCU
-            - 250ms Connecting
-            - 1000ms high /5000ms low Connected
+            - Fast blink: Connecting
+            - Slow blink: Connected
         """
-        connected = sta_if.is_connected()
+        connected = self.is_connected()
+        self._toggle()
         time.sleep_ms(CONFIG['wifi']['status'][connected])
-        self._toggle(self.pin)
+
+    def get_information(self):
+        return {
+            'is_connected': self.is_connected(),
+            'ifconfig': self.sta_if.ifconfig()
+        }
 
     def deepsleep(self, rtc, milliseconds):
         # configure RTC.ALARM0 to be able to wake the device
@@ -92,23 +105,17 @@ class Service(object):
 
 if __name__ == '__main__':
     rtc = RTC()
-
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
     debug = CONFIG['debug']
     service = Service()
+    connected = service.connect()
 
-    connected = False
-    while not connected:
-        service.connect_ap(sta_if)
-        connected = sta_if.is_connected()
-
+    while service.is_connected is False:
+        print("NOT Connected!")
         service.led_status()
-        service.sleep(CONFIG['sleep']['not_connected'])
+        connected = service.connect()
+        if not connected:
+            time.sleep_ms(CONFIG['sleep']['not_connected'])
 
-    while connected:
-        report = service.get_sensor_data()
-        report.notify()
     else:
         while True:
             if debug:
